11b448b1a00e82c5c89a93c0adb1f2d9eda33ebf
Yang Luo <hsluoyz@qq.com>
2018-08-13 12:12:51 +0800

    Parse Apache logs.

---
+++ main.go
@@ -0,0 +1,57 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"os"
+	"bufio"
+	"strings"
+	"github.com/Songmu/axslogparser"
+)
+
+func loadLogFile(filePath string, handler func(string)) error {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		handler(line)
+	}
+	return scanner.Err()
+}
+
+func apacheLogHandler(line string) {
+	if line == "" {
+		return
+	}
+
+	l, err := axslogparser.Parse(line)
+	if err != nil {
+		panic(err)
+	}
+	printApacheLog(l)
+}
+
+func printApacheLog(l *axslogparser.Log) {
+	println(l.Host, l.Time.String(), l.Request, l.Status, l.Referer, l.UserAgent, l.RequestURI, l.Method)
+}
+
+func main() {
+	loadLogFile("log/raith.log", apacheLogHandler)
+}
